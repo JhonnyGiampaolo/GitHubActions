b7c9028dbcd6d97b7c395047387c3f8a631efa75
JhonnyGiampaolo <67201067+JhonnyGiampaolo@users.noreply.github.com>
2023-10-09 15:59:08 -0500

    Create holaMundo.ts

---
+++ holaMundo.ts
@@ -0,0 +1,7 @@
+// Definir una función que imprima "Hola, mundo"
+function imprimirHolaMundo() {
+  console.log("Hola, mundo");
+}
+
+// Llamar a la función para imprimir el mensaje
+imprimirHolaMundo();
